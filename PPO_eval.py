2dd9f727da65ceb1b2a59a8bfaaf1cbdf2208a6d
amirhosein <ah.pourdavoud79@gmail.com>
2025-03-01 23:03:36 +0330

    Refactor environment setup in PPO_eval.py for improved logging and configuration

--- PPO_eval.py
+++ PPO_eval.py
@@ -2,20 +2,23 @@ from typing import Optional
 import gymnasium as gym
 import numpy as np
 from PPO.Agent import Agent
+from PPO.utils import get_unique_log_dir
 
 
 def make_env(gym_id, seed: Optional[int] = None):
     def thunk():
         env = gym.make(gym_id, render_mode="rgb_array")
-        env = gym.wrappers.RecordEpisodeStatistics(env)
-        env = gym.wrappers.RecordVideo(env, "./results/video", lambda x: True)
-        env = gym.wrappers.ClipAction(env)
-        env = gym.wrappers.NormalizeObservation(env)
-        env = gym.wrappers.TransformObservation(
-            env, lambda obs: np.clip(obs, -10, 10), env.observation_space
-        )
-        env = gym.wrappers.NormalizeReward(env)
-        env = gym.wrappers.TransformReward(env, lambda reward: np.clip(reward, -10, 10))
+        # env = gym.wrappers.RecordEpisodeStatistics(env)
+        env_name = gym_id.split("-")[0]
+        chkpt_vd = get_unique_log_dir(f"./results/{env_name}/video/PPO")
+        env = gym.wrappers.RecordVideo(env, chkpt_vd, lambda x: True)
+        # env = gym.wrappers.ClipAction(env)
+        # env = gym.wrappers.NormalizeObservation(env)
+        # env = gym.wrappers.TransformObservation(
+        #     env, lambda obs: np.clip(obs, -10, 10), env.observation_space
+        # )
+        # env = gym.wrappers.NormalizeReward(env)
+        # env = gym.wrappers.TransformReward(env, lambda reward: np.clip(reward, -10, 10))
         if seed is not None:
             env.seed(seed)
             env.action_space.seed(seed)
@@ -26,7 +29,8 @@ def make_env(gym_id, seed: Optional[int] = None):
 
 
 if __name__ == "__main__":
-    env = make_env("BipedalWalker-v3")
+    env_name = "Hopper"
+    env = make_env("Hopper-v5")
     env = env()
 
     n_epochs = 5
@@ -35,9 +39,9 @@ if __name__ == "__main__":
     timeLimit = env.spec.max_episode_steps
 
     policy_kwargs = {
-        "feature": [64, 32],
-        "pi": [256, 256],
-        "vf": [256, 128],
+        "feature": [],
+        "pi": [64, 64],
+        "vf": [64, 64],
     }
 
     agent = Agent(
@@ -46,7 +50,7 @@ if __name__ == "__main__":
         policy_kwargs=policy_kwargs,
         batch_size=batch_size,
         n_epochs=n_epochs,
-        chkpt_dir="./results/models/PPO_13",
+        chkpt_dir=f"./results/{env_name}/models/PPO",
     )
 
     agent.load_models()
@@ -58,7 +62,9 @@ if __name__ == "__main__":
         score = 0
         t_step = 0
         while not done:
-            action, logprob, value = agent.get_action(state)
+            action, logprob, _ = agent.get_action(state)
+            action = action.detach().cpu().numpy().squeeze()
+
             next_state, reward, terminated, truncated, info = env.step(action)
             done = terminated or truncated
             state = next_state
