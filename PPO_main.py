6e76b33c00ef9058c657311bef4b004564fb59bb
amirhosein <ah.pourdavoud79@gmail.com>
2025-03-01 23:03:56 +0330

    Refactor PPO_main.py for improved environment setup and logging structure

--- PPO_main.py
+++ PPO_main.py
@@ -2,20 +2,20 @@ from typing import Optional
 import gymnasium as gym
 import numpy as np
 from PPO.Agent import Agent
-from PPO.utils import plot_learning_curve, Logger, get_unique_log_dir
+from PPO.utils import Logger, get_unique_log_dir
 
 
 def make_env(gym_id, seed: Optional[int] = None):
     def thunk():
         env = gym.make(gym_id)
-        env = gym.wrappers.RecordEpisodeStatistics(env)
-        env = gym.wrappers.ClipAction(env)
-        env = gym.wrappers.NormalizeObservation(env)
-        env = gym.wrappers.TransformObservation(
-            env, lambda obs: np.clip(obs, -10, 10), env.observation_space
-        )
-        env = gym.wrappers.NormalizeReward(env)
-        env = gym.wrappers.TransformReward(env, lambda reward: np.clip(reward, -10, 10))
+        # env = gym.wrappers.RecordEpisodeStatistics(env)
+        # env = gym.wrappers.ClipAction(env)
+        # env = gym.wrappers.NormalizeObservation(env)
+        # env = gym.wrappers.TransformObservation(
+        #     env, lambda obs: np.clip(obs, -10, 10), env.observation_space
+        # )
+        # env = gym.wrappers.NormalizeReward(env)
+        # env = gym.wrappers.TransformReward(env, lambda reward: np.clip(reward, -10, 10))
         if seed is not None:
             env.seed(seed)
             env.action_space.seed(seed)
@@ -26,27 +26,28 @@ def make_env(gym_id, seed: Optional[int] = None):
 
 
 if __name__ == "__main__":
-    env = make_env("BipedalWalker-v3")
+    env_name = "Hopper"
+    env = make_env("Hopper-v5")
     env = env()
 
-    n_epochs = 15
-    batch_size = 32
-    episode_num = 4000
-    training_interval_step = 200
+    n_epochs = 10
+    batch_size = 64
+    episode_num = 5000
+    training_interval_step = 1024
     timeLimit = env.spec.max_episode_steps
 
-    chkpt_dir = "./results/models/PPO"
+    chkpt_dir = f"./results/{env_name}/models/PPO"
     chkpt_dir = get_unique_log_dir(chkpt_dir)
 
-    figure_folder = "./results/plots/PPO"
+    figure_folder = f"./results/{env_name}/plots/PPO"
     figure_file = get_unique_log_dir(figure_folder) + "/LearningCurve.png"
 
-    logger = Logger(log_dir="./results/tb/PPO")
+    logger = Logger(log_dir=f"./results/{env_name}/tb/PPO")
 
     policy_kwargs = {
-        "feature": [64, 32],
-        "pi": [256, 256],
-        "vf": [256, 128],
+        "feature": [],
+        "pi": [64, 64],
+        "vf": [64, 64],
     }
 
     agent = Agent(
@@ -73,6 +74,7 @@ if __name__ == "__main__":
         score = 0
         t_step = 0
         while not done:
+            agent.policy.eval()
             action, logprob, value = agent.get_action(state)
             action = action.detach().cpu().numpy().squeeze()
             logprob = logprob.item()
@@ -94,7 +96,7 @@ if __name__ == "__main__":
             n_steps += 1
             t_step += 1
             logger.update_global_step(n_steps)
-            logger.add_scalar("reward", reward)
+            logger.add_scalar("rollout/step_reward", reward, n_steps)
 
             if n_steps % training_interval_step == 0:
                 agent.anneal_lr(current_step=learn_iters, total_steps=learn_steps)
@@ -103,11 +105,11 @@ if __name__ == "__main__":
             state = next_state
 
         score_history.append(score)
-        avg_score = np.mean(score_history[-100:])
+        avg_score = np.mean(score_history[-10:])
 
-        logger.add_scalar("episodic mean reward", score / t_step, n_steps)
-        logger.add_scalar("avg score", avg_score, n_steps)
-        logger.add_scalar("charts/rollout_step", t_step, n_steps)
+        logger.add_scalar("rollout/avg score", avg_score, n_steps)
+        logger.add_scalar("rollout/episode_reward", score, n_steps)
+        logger.add_scalar("rollout/episode_len", t_step, n_steps)
 
         if avg_score > best_score:
             best_score = avg_score
@@ -115,7 +117,7 @@ if __name__ == "__main__":
 
         print(
             "episode",
-            eps,
+            eps + 1,
             "score %.1f" % score,
             "avg score %.1f" % avg_score,
             "global_steps",
@@ -123,5 +125,3 @@ if __name__ == "__main__":
             "episode_step",
             t_step,
         )
-    x = [i + 1 for i in range(len(score_history))]
-    plot_learning_curve(x, score_history, figure_file)
