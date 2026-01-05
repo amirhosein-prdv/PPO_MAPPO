3e94c46a904a34927783041e16b41524bef1e6d7
Amirhosein <ah.pourdavoud79@gmail.com>
2026-01-05 18:59:03 +0330

    Refactor Agent: remove max_action parameter, update constructor for gamma and gae_lambda, and enhance learning process with improved logging

--- PPO/Agent.py
+++ PPO/Agent.py
@@ -1,6 +1,8 @@
 import numpy as np
 import torch as T
-from typing import Optional, Tuple, List
+from torch.nn import functional as F
+from torch.distributions.normal import Normal
+from typing import Optional, Tuple, List, Union
 
 from .RolloutBuffer import RolloutBuffer
 from .Networks import ActorNetwork, CriticNetwork, ActorCriticNetwork
@@ -13,34 +15,35 @@ class Agent:
         self,
         state_dim: int,
         action_dim: int,
-        max_action: int,
         batch_size: int = 64,
         n_epochs: int = 10,
         gamma: float = 0.99,
         gae_lambda: float = 0.95,
         lr: float = 3e-4,
-        clip_coef: float = 0.2,
+        clip_range: float = 0.2,
+        clip_range_vf: Union[None, float] = None,
         vf_coef: float = 0.5,
-        ent_coef: float = 0.0,
+        ent_coef: float = 0.01,
         max_grad_norm: float = 0.5,
         target_kl: Optional[float] = None,
+        normalize_advantage: bool = True,
         logger: Optional["Logger"] = None,
         policy_kwargs: dict[str, List[int]] = {
-            "feature": [32],
+            "feature": [],
             "pi": [64, 64],
             "vf": [64, 64],
         },
         chkpt_dir: str = "./tmp/PPO-Agent",
         model_name: str = "actor_critic_ppo",
     ) -> None:
-        self.gamma = gamma
-        self.gae_lambda = gae_lambda
 
-        self.clip_coef = clip_coef
+        self.clip_range = clip_range
+        self.clip_range_vf = clip_range_vf
         self.ent_coef = ent_coef
         self.vf_coef = vf_coef
         self.max_grad_norm = max_grad_norm
         self.target_kl = target_kl
+        self.normalize_advantage = normalize_advantage
 
         self.n_epochs = n_epochs
         self.logger = logger
@@ -54,9 +57,6 @@ class Agent:
         #         ),
         #     )
 
-        self.norm_adv = True
-        self.clip_vloss = True
-
         feature_net = policy_kwargs["feature"]
         vf_net = policy_kwargs["vf"]
         pi_net = policy_kwargs["pi"]
@@ -64,7 +64,6 @@ class Agent:
         self.policy = ActorCriticNetwork(
             state_dim,
             action_dim,
-            max_action=max_action,
             feature_fc_dims=feature_net,
             actor_fc_dims=pi_net,
             critic_fc_dims=vf_net,
@@ -73,7 +72,7 @@ class Agent:
             model_name=model_name,
         )
 
-        self.memory = RolloutBuffer(batch_size)
+        self.memory = RolloutBuffer(batch_size, gamma, gae_lambda)
         self.device = self.policy.device
 
     def anneal_lr(self, current_step: int, total_steps: int) -> None:
@@ -82,139 +81,77 @@ class Agent:
             self.policy.optimizer, self.policy.initial_lr, current_step, total_steps
         )
 
-    def get_value(self, observation: np.ndarray) -> float:
-        state = T.tensor(np.array([observation]), dtype=T.float32).to(self.device)
-        value = self.policy(state)[1]
-        return value
-
-    def get_action(
-        self, observation: np.ndarray, action: Optional[T.Tensor] = None
-    ) -> Tuple[np.ndarray, float, float]:
-        state = T.tensor(np.array([observation]), dtype=T.float32).to(self.device)
-        dist, value = self.policy(state)
-
-        if action is None:
-            action = dist.sample()
-        logprobs = dist.log_prob(action).sum(-1)
-
-        return action, logprobs, value
-
-    def get_GAE_and_returns(self, reward_arr, values_arr, dones_arr, next_states_arr):
-        advantages = np.zeros(len(reward_arr), dtype=np.float32)
-        returns = np.zeros(len(reward_arr), dtype=np.float32)
-        with T.no_grad():
-            # Advantage for last step
-            last_value = self.get_value(next_states_arr[-1]).item()
-            delta_last = (
-                reward_arr[-1]
-                + self.gamma * last_value * (1 - int(dones_arr[-1]))
-                - values_arr[-1]
-            )
-            advantages[-1] = delta_last
-
-            # Calculate advantages for the values in memory (bootstrap)
-            for t in range(len(reward_arr) - 1):
-                discount = 1
-                a_t = 0
-                for k in range(t, len(reward_arr) - 1):
-                    delta = (
-                        reward_arr[k]
-                        + self.gamma * values_arr[k + 1] * (1 - int(dones_arr[k]))
-                        - values_arr[k]
-                    )
-                    a_t += discount * delta
-                    discount *= self.gamma * self.gae_lambda
-                    if dones_arr[k]:
-                        break
-                advantages[t] = a_t + discount * advantages[k + 1]
-
-            returns = advantages + values_arr
-
-        return advantages, returns
-
     def learn(self) -> None:
         # Generate Data
         (
             state_arr,
             action_arr,
-            old_prob_arr,
+            old_logprob_arr,
             values_arr,
-            next_states_arr,
-            reward_arr,
-            dones_arr,
+            advantages_arr,
+            returns_arr,
         ) = self.memory.get_data()
 
-        # Calculate Advantages
-        advantages, returns = self.get_GAE_and_returns(
-            reward_arr, values_arr, dones_arr, next_states_arr
-        )
-
-        returns = T.tensor(returns, dtype=T.float32).to(self.device)
-        advantages = T.tensor(advantages, dtype=T.float32).to(self.device)
-        values = T.tensor(values_arr, dtype=T.float32).to(self.device)
+        returns = T.tensor(returns_arr, dtype=T.float32).squeeze().to(self.device)
+        advantages = T.tensor(advantages_arr, dtype=T.float32).squeeze().to(self.device)
+        values = T.tensor(values_arr, dtype=T.float32).squeeze().to(self.device)
 
         clipfracs = []
         critic_buffer = []
         actor_buffer = []
         entropy_buffer = []
         approx_kl_buffer = []
-        explained_var_buffer = []
         self.policy.train()
         for _ in range(self.n_epochs):
-            # Generate batch data
+            # Generate minibatch data
             batches = self.memory.generate_batches()
 
             # Training
-            for batch in batches:
-                states = T.tensor(state_arr[batch], dtype=T.float).to(self.device)
-                old_probs = T.tensor(old_prob_arr[batch]).to(self.device)
-                actions = T.tensor(action_arr[batch]).to(self.device)
-
-                dist, new_value = self.policy(states)
-                new_value = T.squeeze(new_value)
-
-                dist_entropy = dist.entropy().sum(1, keepdim=True)
-                new_probs = dist.log_prob(actions).sum(1)
-                prob_ratio = (new_probs - old_probs).exp()
-                prob_logratio = new_probs - old_probs
-
-                with T.no_grad():
-                    # calculate approx_kl http://joschu.net/blog/kl-approx.html
-                    approx_kl = ((prob_ratio - 1) - prob_logratio).mean()
-                    clipfracs += [
-                        ((prob_ratio - 1.0).abs() > self.clip_coef)
-                        .float()
-                        .mean()
-                        .item()
-                    ]
+            for minibatch in batches:
+                old_logprob = (
+                    T.tensor(old_logprob_arr[minibatch]).squeeze().to(self.device)
+                )
+                new_value, new_logprob, entropy = self.policy.evaluate_action(
+                    state_arr[minibatch], action_arr[minibatch]
+                )
+                new_value = new_value.flatten()
+                prob_ratio = T.exp(new_logprob - old_logprob)
+                prob_logratio = new_logprob - old_logprob
 
                 # nomalize advantage
-                normalized_advantages = (
-                    advantages[batch] - advantages[batch].mean()
-                ) / (advantages[batch].std() + 1e-8)
+                if self.normalize_advantage:
+                    normalized_advantages = (
+                        advantages[minibatch] - advantages[minibatch].mean()
+                    ) / (advantages[minibatch].std() + 1e-8)
+                else:
+                    normalized_advantages = advantages[minibatch]
 
                 # calculate actor loss
                 weighted_probs = normalized_advantages * prob_ratio
                 weighted_clipped_probs = (
-                    T.clamp(prob_ratio, 1 - self.clip_coef, 1 + self.clip_coef)
+                    T.clamp(prob_ratio, 1 - self.clip_range, 1 + self.clip_range)
                     * normalized_advantages
                 )
                 actor_loss = -T.min(weighted_probs, weighted_clipped_probs).mean()
 
                 # Calculate Critic loss
-                # clip value loss
-                clipped_value = values[batch] + T.clamp(
-                    new_value - values[batch],
-                    -self.clip_coef,
-                    self.clip_coef,
-                )
-                critic_loss_clipped = (returns[batch] - clipped_value) ** 2
-                critic_loss_unclipped = (returns[batch] - new_value) ** 2
-                critic_loss_max = T.max(critic_loss_clipped, critic_loss_unclipped)
-                critic_loss = 0.5 * critic_loss_max.mean()
+                if self.clip_range_vf is None:
+                    critic_loss = F.mse_loss(returns[minibatch], new_value)
+                else:
+                    # clip value loss
+                    clipped_value = values[minibatch] + T.clamp(
+                        new_value - values[minibatch],
+                        -self.clip_range_vf,
+                        self.clip_range_vf,
+                    )
+
+                    critic_loss_clipped = (returns[minibatch] - clipped_value) ** 2
+                    critic_loss_unclipped = (returns[minibatch] - new_value) ** 2
+                    critic_loss_max = T.max(critic_loss_clipped, critic_loss_unclipped)
+                    critic_loss = 0.5 * critic_loss_max.mean()
 
                 # Calculate entropy loss
-                entropy_loss = dist_entropy.mean()
+                entropy_loss = entropy.mean()
 
                 total_loss = (
                     actor_loss
@@ -222,42 +159,51 @@ class Agent:
                     - self.ent_coef * entropy_loss
                 )
 
+                with T.no_grad():
+                    # calculate approx_kl http://joschu.net/blog/kl-approx.html
+                    approx_kl = T.mean((prob_ratio - 1) - prob_logratio).cpu().numpy()
+                    clipfracs += [
+                        ((prob_ratio - 1.0).abs() > self.clip_range)
+                        .float()
+                        .mean()
+                        .item()
+                    ]
+
+                if self.target_kl is not None:
+                    if approx_kl > self.target_kl:
+                        break
+
                 self.policy.optimizer.zero_grad()
                 total_loss.backward()
                 T.nn.utils.clip_grad_norm_(self.policy.parameters(), self.max_grad_norm)
                 self.policy.optimizer.step()
-
-                # Calculate explained variance
-                y_pred, y_true = values.cpu().numpy(), returns.cpu().numpy()
-                var_y = np.var(y_true)
-                explained_var = (
-                    np.nan if var_y == 0 else 1 - np.var(y_true - y_pred) / var_y
-                )
 
                 # Store training variable to log
                 critic_buffer.append(critic_loss.item())
                 actor_buffer.append(actor_loss.item())
                 entropy_buffer.append(entropy_loss.item())
                 approx_kl_buffer.append(approx_kl.item())
-                explained_var_buffer.append(explained_var)
 
-            if self.target_kl is not None:
-                if approx_kl > self.target_kl:
-                    break
+        # Calculate explained variance
+        y_pred, y_true = (
+            values.cpu().numpy(),
+            returns.cpu().numpy(),
+        )
+        var_y = np.var(y_true)
+        explained_var = np.nan if var_y == 0 else 1 - np.var(y_true - y_pred) / var_y
 
         if self.logger is not None:
             # record for plotting purposes
             self.logger.add_scalar(
                 "train/learning_rate", self.policy.optimizer.param_groups[0]["lr"]
             )
+            self.logger.add_scalar("train/loss", total_loss.item())
             self.logger.add_scalar("train/value_loss", np.mean(critic_buffer))
-            self.logger.add_scalar("train/policy_loss", np.mean(actor_buffer))
+            self.logger.add_scalar("train/policy_gradient_loss", np.mean(actor_buffer))
             self.logger.add_scalar("train/entropy", np.mean(entropy_buffer))
             self.logger.add_scalar("train/approx_kl", np.mean(approx_kl_buffer))
-            self.logger.add_scalar("train/clipfrac", np.mean(clipfracs))
-            self.logger.add_scalar(
-                "train/explained_variance", np.mean(explained_var_buffer)
-            )
+            self.logger.add_scalar("train/clip_fraction", np.mean(clipfracs))
+            self.logger.add_scalar("train/explained_variance", explained_var)
 
         self.memory.clear()
 
