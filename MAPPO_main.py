7b07d6141fd1de0601df9b074b7b8353671b3ca4
Amirhosein <ah.pourdavoud79@gmail.com>
2026-01-06 15:44:37 +0330

    Refactor: Update training script with changes in MAPPO algorithm

--- MAPPO_main.py
+++ MAPPO_main.py
@@ -34,8 +34,6 @@ if __name__ == "__main__":
     # eval_env = make_env("BipedalWalker-v3")
     # eval_env = eval_env()
 
-    n_epochs = 15
-    batch_size = 32
     episode_num = 4000
     training_interval_step = 200
     timeLimit = env.spec.max_episode_steps
@@ -43,8 +41,8 @@ if __name__ == "__main__":
     chkpt_dir = "./results/models/PPO"
     chkpt_dir = get_unique_log_dir(chkpt_dir)
 
-    figure_folder = "./results/plots/PPO"
-    figure_file = get_unique_log_dir(figure_folder) + "/LearningCurve.png"
+    # figure_folder = "./results/plots/PPO"
+    # figure_file = get_unique_log_dir(figure_folder) + "/LearningCurve.png"
 
     logger = Logger(log_dir="./results/tb/PPO")
 
@@ -56,10 +54,10 @@ if __name__ == "__main__":
 
     multiAgents = MultiAgent(
         env=env,
-        policy_kwargs=policy_kwargs,
-        batch_size=batch_size,
-        n_epochs=n_epochs,
+        batch_size=32,
+        n_epochs=15,
         logger=logger,
+        policy_kwargs=policy_kwargs,
         chkpt_dir=chkpt_dir,
     )
 
@@ -68,6 +66,7 @@ if __name__ == "__main__":
 
     best_score = -np.inf
     score_history = []
+    reward_history = []
 
     learn_steps = timeLimit * episode_num // training_interval_step
     learn_iters = 0
@@ -75,29 +74,26 @@ if __name__ == "__main__":
 
     n_steps = 0
     for eps in range(episode_num):
-        state, infos = env.reset()
-        done = False
+        states, infos = env.reset()
+        last_done = False
+        last_dones = {agent: False for agent in env.agents}
         score = 0
         t_step = 0
-        while not done:
+        while not last_done:
             multiAgents.eval()
-            action, logprob, value = multiAgents.get_action(state)
-            action = {k: v.detach().cpu().numpy().squeeze() for k, v in action.items()}
-            logprob = {k: v.item() for k, v in logprob.items()}
-            value = {k: v.item() for k, v in value.items()}
+            actions, values, logprobs = multiAgents.get_actions(states)
+            actions = {
+                k: v.detach().squeeze().cpu().numpy() for k, v in actions.items()
+            }
+            logprobs = {k: v.detach().cpu().numpy() for k, v in logprobs.items()}
+            values = {k: v.detach().squeeze(0).cpu().numpy() for k, v in values.items()}
 
-            next_state, rewards, terminations, truncations, infos = env.step(action)
+            next_states, rewards, terminations, truncations, infos = env.step(actions)
+            dones = {
+                agent: terminations[agent] or truncations[agent] for agent in env.agents
+            }
             done = any(terminations.values()) or any(truncations.values())
             score += sum([v for v in rewards.values()])
-            multiAgents.memory.store(
-                state,
-                action,
-                logprob,
-                value,
-                next_state,
-                rewards,
-                done,
-            )
 
             n_steps += 1
             t_step += 1
@@ -105,21 +101,46 @@ if __name__ == "__main__":
             logger.update_global_step(n_steps)
             logger.add_dict("rollout/step_rewards", rewards, n_steps)
 
+            multiAgents.memory.store(
+                states,
+                actions,
+                logprobs,
+                values,
+                rewards,
+                last_dones,
+            )
+
             if n_steps % training_interval_step == 0:
                 multiAgents.anneal_lr(current_step=learn_iters, total_steps=learn_steps)
+                last_values = multiAgents.get_values(next_states)
+                last_values = {
+                    k: v.detach().squeeze(0).cpu().numpy()
+                    for k, v in last_values.items()
+                }
+                multiAgents.memory.compute_GAE_and_returns(
+                    last_value=last_values, done=dones
+                )
                 multiAgents.learn()
                 # eval_logger.evaluate_and_log()
                 learn_iters += 1
-            state = next_state
+            states = next_states
+            last_dones = dones
+            last_done = done
 
         # episode_logger.record_log()
 
         score_history.append(score)
         avg_score = np.mean(score_history[-10:])
 
-        logger.add_scalar("rollout/avg score", avg_score, n_steps)
-        logger.add_scalar("rollout/episode_rewards", score / t_step, n_steps)
-        logger.add_scalar("rollout/episode_len", t_step, n_steps)
+        mean_reward = score / t_step
+        reward_history.append(mean_reward)
+        avg_reward = np.mean(reward_history[-10:])
+
+        logger.add_scalar("rollout/reward", mean_reward, n_steps)
+        logger.add_scalar("rollout/avg_reward", avg_reward, n_steps)
+        logger.add_scalar("rollout/ep_rew", score, n_steps)
+        logger.add_scalar("rollout/ep_rew_mean", avg_score, n_steps)
+        logger.add_scalar("rollout/ep_len_mean", t_step, n_steps)
 
         if avg_score > best_score:
             best_score = avg_score
@@ -135,5 +156,6 @@ if __name__ == "__main__":
             "episode_step",
             t_step,
         )
-    x = [i + 1 for i in range(len(score_history))]
-    plot_learning_curve(x, score_history, figure_file)
+
+    # x = [i + 1 for i in range(len(score_history))]
+    # plot_learning_curve(x, score_history, figure_file)
